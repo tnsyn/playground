8519b08380db7461f3fa4ae2a798b5b7c1f650b8
Tan Su Yin <e0425292@u.nus.edu>
2021-08-05 18:11:13 +0800

    Change from varying test size to varying train size

--- api/api.py
+++ api/api.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/api/knn/user-based-common-movies/random-user', methods=['POST'])
 def get_favourite_genre():
   k = json.loads(request.data)['k_value']
-  test_size = json.loads(request.data)['test_size']
+  test_size = 100 - json.loads(request.data)['train_size']
   ratings = json.loads(request.data)['ratings']
   genre = predict_subsequent_random_users(k, test_size, ratings)
   return {'genre': genre}
@@ -16,7 +16,7 @@ def get_favourite_genre():
 @app.route('/api/knn/user-based-common-movies', methods=['POST'])
 def run_model_and_predict():
   k = json.loads(request.data)['k_value']
-  test_size = json.loads(request.data)['test_size']
+  test_size = 100 - json.loads(request.data)['train_size']
   ratings = json.loads(request.data)['ratings']
   results = apply_knn_user_based_common_movies(k, test_size, ratings)
   return results
